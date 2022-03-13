976a4bd1a2b23eda47f68436440d34c774529d91
TDL2627 <the.dark.lord.2627@gmail.com>
2022-03-13 14:26:19 +0200

    added content to shuttle and mentor

--- routes/mentorRoute.js
+++ routes/mentorRoute.js
@@ -6,33 +6,39 @@ router.get("/", function(req, res){
     let mentors=[
         {name:"Zainunesa Magmoed",
         pic:"mentor.png",
+        qualification:"Nautical Science",
+        number:`<a  target="_blank" href="https://wa.me/27660400809"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
+        email:"zainunesa@gmail.com"
+        },
+        {name:"Ulf Krohne",
+        pic:"mentor.png",
+        qualification:"Nautical Science",
+        number:`<a  target="_blank" href="https://wa.me/27660400809"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
+        email:"ulf@gmail.com"
+        },
+        {name:"Odirile Madiba",
+        pic:"mentor.png",
+        qualification:"Nautical Science",
+        number:`<a  target="_blank" href="https://wa.me/27660400809"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
+        email:"madiba@gmail.com"
+        },
+        {name:"Sinenhlanhla Luthuli",
+        pic:"mentor.png",
+        qualification:"Nautical Science",
+        number:`<a  target="_blank" href="https://wa.me/27660400809"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
+        email:"luthu@gmail.com"
+        },
+        {name:"Minenhle Khuzwayo",
+        pic:"mentor.png",
+        qualification:"Nautical Science",
         number:`<a  target="_blank" href="https://wa.me/27660400809"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
         email:"zainunesamagmoed1@gmail.com"
         },
-        {name:"Zainunesa Magmoed",
+        {name:"Japhet Mbila",
         pic:"mentor.png",
+        qualification:"Marine Engineering",
         number:`<a  target="_blank" href="https://wa.me/27660400809"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
-        email:"zainunesamagmoed1@gmail.com"
-        },
-        {name:"Zainunesa Magmoed",
-        pic:"mentor.png",
-        number:`<a  target="_blank" href="https://wa.me/27660400809"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
-        email:"zainunesamagmoed1@gmail.com"
-        },
-        {name:"Zainunesa Magmoed",
-        pic:"mentor.png",
-        number:`<a  target="_blank" href="https://wa.me/27660400809"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
-        email:"zainunesamagmoed1@gmail.com"
-        },
-        {name:"Zainunesa Magmoed",
-        pic:"mentor.png",
-        number:`<a  target="_blank" href="https://wa.me/27660400809"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
-        email:"zainunesamagmoed1@gmail.com"
-        },
-        {name:"Zainunesa Magmoed",
-        pic:"mentor.png",
-        number:`<a  target="_blank" href="https://wa.me/27660400809"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
-        email:"zainunesamagmoed1@gmail.com"
+        email:"mbila@gmail.com"
         },
       ]
       res.send(mentors)
