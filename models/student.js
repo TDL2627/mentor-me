4b0bed706fa94afe9b2cca260c311b92ecceca2b
TDL2627 <the.dark.lord.2627@gmail.com>
2022-03-22 13:22:13 +0200

    added role to student model

--- models/student.js
+++ models/student.js
@@ -18,7 +18,10 @@ const studentSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
- 
+  role: {
+    type: String,
+    required: false
+  },
   subject: {
     type: String,
     required: true,
