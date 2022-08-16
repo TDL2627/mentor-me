9a7f61550685eddfbf367639c4ba0118bca2382c
Ashley Kannemeyer <60740073+TDL2627@users.noreply.github.com>
2022-08-16 08:54:03 +0200

    update

--- routes/mentorRoute.js
+++ routes/mentorRoute.js
@@ -2,46 +2,53 @@ var express = require("express");
 const router = express.Router();
 
 // route for time management
-router.get("/", function(req, res){
-    let mentors=[
-        {name:"Zainunesa Magmoed",
-        pic:"nohead.jpeg",
-        qualification:"Nautical Science",
-        number:`<a  target="_blank" href="https://wa.me/27818373613"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
-        email:"zainunesa.magmoed1@gmail.com"
-        },
-        {name:"Ulf Krohne",
-        pic:"elf.jpeg",
-        qualification:"Nautical Science",
-        number:`<a  target="_blank" href="https://wa.me/264813938153"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
-        email:"ulf@gmail.com"
-        },
-        {name:"Odirile Madiba",
-        pic:"roar.jpeg",
-        qualification:"Nautical Science",
-        number:`<a  target="_blank" href="https://wa.me/27737605438"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
-        email:"madiba@gmail.com"
-        },
-        {name:"Sinenhhlanhla Luthuli",
-        pic:"haha.jpeg",
-        qualification:"Nautical Science",
-        number:`<a  target="_blank" href="https://wa.me/27846145920"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
-        email:"luthu@gmail.com"
-        },
-        {name:"Nokwanda Sibiya",
-        pic:"haha.jpeg",
-        qualification:"Nautical Science",
-        number:`<a  target="_blank" href="https://wa.me/27676119341"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
-        email:"luthu@gmail.com"
-        },
-        {name:"Japhet Mbila",
-        pic:"mentor.png",
-        qualification:"Marine Engineering",
-        number:`<a  target="_blank" href="https://wa.me/27660400809"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
-        email:"mbila@gmail.com"
-        },
-      ]
-      res.send(mentors)
- });
+router.get("/", function (req, res) {
+  let mentors = [
+    {
+      name: "Zainunesa Magmoed",
+      pic: "nohead.jpeg",
+      qualification: "Nautical Science",
+      number: `<a  target="_blank" href="https://wa.me/27818373613"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
+      email: "zainunesa.magmoed1@gmail.com",
+    },
+
+    {
+      name: "Odirile Madiba",
+      pic: "roar.jpeg",
+      qualification: "Nautical Science",
+      number: `<a  target="_blank" href="https://wa.me/27737605438"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
+      email: "madiba@gmail.com",
+    },
+    {
+      name: "Ulf Krohne",
+      pic: "elf.jpeg",
+      qualification: "Nautical Science",
+      number: `<a  target="_blank" href="https://wa.me/264813938153"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
+      email: "ulf@gmail.com",
+    },
+    {
+      name: "Sinenhhlanhla Luthuli",
+      pic: "haha.jpeg",
+      qualification: "Nautical Science",
+      number: `<a  target="_blank" href="https://wa.me/27846145920"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
+      email: "luthu@gmail.com",
+    },
+    {
+      name: "Nokwanda Sibiya",
+      pic: "wakanda.jpeg",
+      qualification: "Nautical Science",
+      number: `<a  target="_blank" href="https://wa.me/27676119341"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
+      email: "luthu@gmail.com",
+    },
+    {
+      name: "Japhet Mbila",
+      pic: "mentor.png",
+      qualification: "Marine Engineering",
+      number: `<a  target="_blank" href="https://wa.me/27660400809"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
+      email: "mbila@gmail.com",
+    },
+  ];
+  res.send(mentors);
+});
 
 module.exports = router;
