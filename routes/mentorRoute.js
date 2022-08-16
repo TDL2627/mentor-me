5216f184bec26b242e38d3ecb6fe29d01114fca0
Ashley Kannemeyer <60740073+TDL2627@users.noreply.github.com>
2022-08-16 09:06:34 +0200

    update

--- routes/mentorRoute.js
+++ routes/mentorRoute.js
@@ -40,13 +40,13 @@ router.get("/", function (req, res) {
       number: `<a  target="_blank" href="https://wa.me/27676119341"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
       email: "luthu@gmail.com",
     },
-    {
-      name: "Japhet Mbila",
-      pic: "mentor.png",
-      qualification: "Marine Engineering",
-      number: `<a  target="_blank" href="https://wa.me/27660400809"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
-      email: "mbila@gmail.com",
-    },
+    // {
+    //   name: "Japhet Mbila",
+    //   pic: "mentor.png",
+    //   qualification: "Marine Engineering",
+    //   number: `<a  target="_blank" href="https://wa.me/27660400809"><img src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-whatsapp-social-media-justicon-flat-justicon.png"/></a>`,
+    //   email: "mbila@gmail.com",
+    // },
   ];
   res.send(mentors);
 });
